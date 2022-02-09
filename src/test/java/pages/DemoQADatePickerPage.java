fc74fad0f5ba2312e2e66685afcf1685bc7a3ce1
GulpariA <gulpari9211@gmail.com>
2022-02-08 20:34:53 -0500

    Added maven properties and plugins

--- src/test/java/pages/DemoQADatePickerPage.java
+++ src/test/java/pages/DemoQADatePickerPage.java
@@ -10,36 +10,59 @@ import utilities.BrowserUtils;
 import utilities.Driver;
 
 public class DemoQADatePickerPage {
+
 	public DemoQADatePickerPage() {
 		PageFactory.initElements(Driver.getDriver(), this);
 	}
 	
-	@FindBy(id="datePickerMonthYearInput")
+	@FindBy (id= "datePickerMonthYearInput")
 	public WebElement firstDatePicker;
 	
-	@FindBy(css = ".react-datepicker__year-select")
+	@FindBy (css = ".react-datepicker__year-select")
 	public WebElement yearDropDown;
 	
-	@FindBy(css = ".react-datepicker__month-select")
+	@FindBy (css = ".react-datepicker__month-select")
 	public WebElement monthDropDown;
 	
-	@FindBy(css = ".react-datepicker__day")
+	@FindBy (css = ".react-datepicker__day")
 	public List<WebElement> days;
 	
-	BrowserUtils utils= new BrowserUtils();
 	
-	public void pickADate(String year, int monthIndex, String day) {
+	BrowserUtils utils = new BrowserUtils();
+	
+	int month;
+	String day;
+	String year;
+	
+	public void pickADate(String date) {
+		
+		String[] dates = date.split("/");  // 12/11/2006
+	    // turn the month into int
+	    String monthOutofArray = dates[0];
+	    if (monthOutofArray.contains("0")) {
+	    	month = Integer.parseInt(monthOutofArray.substring(1));
+	    }else {
+	    	month = Integer.parseInt(monthOutofArray);
+	    }
+	    
+	    // get the date out of string array
+	    if (dates[1].contains("0")) {
+	    	day = dates[1].substring(1);
+	    }else {
+	    	day = dates[1];
+	    }
+	    // year from the string array
+	    year = dates[2];
+		
 		utils.selectByValue(yearDropDown, year);
-		utils.selectByIndex(monthDropDown,monthIndex);
+		utils.selectByIndex(monthDropDown, month -1);
 		
 		for (WebElement webElement : days) {
-			if(webElement.getText().equals(day)) {
+			if (webElement.getText().equals(day)) {
 				webElement.click();
 				break;
-				
 			}
 		}
-		
+	}
 	
-	}	
 }
--- src/test/java/step_definitions/DemoQADatePickerSteps.java
+++ src/test/java/step_definitions/DemoQADatePickerSteps.java
@@ -1,7 +1,6 @@
 package step_definitions;
 
 import org.junit.Assert;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -10,49 +9,35 @@ import utilities.BrowserUtils;
 import utilities.Driver;
 
 public class DemoQADatePickerSteps {
+
 	DemoQADatePickerPage datePickerPage = new DemoQADatePickerPage();
-
 	BrowserUtils utils = new BrowserUtils();
-
+	
+	
 	@Given("I am on the date picker page {string}")
 	public void i_am_on_the_date_picker_page(String url) {
-		Driver.getDriver().get(url);
-		Assert.assertTrue(datePickerPage.firstDatePicker.isDisplayed());
-
+	    Driver.getDriver().get(url);
+	    Assert.assertTrue(datePickerPage.firstDatePicker.isDisplayed());
 	}
-
 	@When("I enter {string} to date picker")
 	public void i_enter_to_date_picker(String date) {
-		utils.clearValueOnTheField(datePickerPage.firstDatePicker);
-		// Sending the date input
-		// datePickerPage.firstDatePicker.sendKeys(date);
-		// pick a date
-		datePickerPage.firstDatePicker.click();
-		String[] dates = date.split("/");
-		String monthOutofArray = dates[0];
-		int month = 0;
-		if (monthOutofArray.contains("0")) {
-			month = Integer.parseInt(monthOutofArray.substring(1));
-		} else {
-			month = Integer.parseInt(monthOutofArray);
-		}
-		
-		String day = "";
-		if (dates[1].contains("0")) {
-			day = dates[1].substring(1);
-		} else {
-			day = dates[1];
-		}
-		
-		String year = dates[2];
-		
-		datePickerPage.pickADate(year, month-1, day);
+	    utils.clearValueOnTheField(datePickerPage.firstDatePicker);
+	    
+	    // first approach is to sending the date into input
+	  // datePickerPage.firstDatePicker.sendKeys(date);
+	    
+	    ////// second approach is to pick a date
+	    
+	    // click on the date element
+	    datePickerPage.firstDatePicker.click();
+	   
+	    // split the date into array of strings
+	    datePickerPage.pickADate(date);
+	    
+	    
 	}
-
 	@Then("Date entered should be selected")
 	public void date_entered_should_be_selected() {
-		datePickerPage.firstDatePicker.getAttribute("value");
-
+		System.out.println("Entered date is: " + datePickerPage.firstDatePicker.getAttribute("value"));
 	}
-
 }
